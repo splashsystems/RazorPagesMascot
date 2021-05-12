457d166375bbe666191c85cbb0d1e14e7f5e0ac2
Scot <bscot.hagen@gmail.com>
2021-05-12 16:30:10 -0700

    Add project files.

---
+++ RazorPagesMascot/Models/Mascot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesMascot.Models
+{
+    public class Mascot
+    {
+        public int ID { get; set; }
+        public string MascotName { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateBegan { get; set; }
+        public string School { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
