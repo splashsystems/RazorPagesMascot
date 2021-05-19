a80810230b3eeadcba2764b651cbc2f7e60dae63
Scot <bscot.hagen@gmail.com>
2021-05-19 15:28:48 -0700

    "finished"

--- RazorPagesMascot/Migrations/20210519182830_New_DataAnnotations.Designer.cs
+++ RazorPagesMascot/Migrations/20210519182830_New_DataAnnotations.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMascot.Data;
 
 namespace RazorPagesMascot.Migrations
 {
     [DbContext(typeof(RazorPagesMascotContext))]
-    partial class RazorPagesMascotContextModelSnapshot : ModelSnapshot
+    [Migration("20210519182830_New_DataAnnotations")]
+    partial class New_DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RazorPagesMascot/Migrations/20210519182830_New_DataAnnotations.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPagesMascot.Migrations
+{
+    public partial class New_DataAnnotations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "School",
+                table: "Mascot",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Rating",
+                table: "Mascot",
+                maxLength: 5,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MascotName",
+                table: "Mascot",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "School",
+                table: "Mascot",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Rating",
+                table: "Mascot",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 5);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MascotName",
+                table: "Mascot",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 60);
+        }
+    }
+}
